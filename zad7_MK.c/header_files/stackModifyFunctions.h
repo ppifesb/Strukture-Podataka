4633d5dc7e06f7826a15e5ce601f39c54a844cd3
ppifesb <56430071+ppifesb@users.noreply.github.com>
2019-12-09 22:00:55 +0100

    Create stackModifyFunctions.h

---
+++ zad7_MK.c/header_files/stackModifyFunctions.h
@@ -0,0 +1,10 @@
+#ifndef STACK_MODIFY_FUNCTIONS
+#define STACK_MODIFY_FUNCTIONS
+
+_Stack* Allocation(_Stack* Element);
+_Stack* Linker(_Stack* previousElement, _Stack* nextElement);
+int Pop(_Stack* head);
+int Push(_Stack* head, int buffer);
+int StoreResultInStack(_Stack* head, int buffer, int oprationNumber);
+
+#endif
