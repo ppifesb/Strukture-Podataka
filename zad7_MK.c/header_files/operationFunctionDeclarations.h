2619a4134ef3dedad859f3b5cae06284557d685f
ppifesb <56430071+ppifesb@users.noreply.github.com>
2019-12-09 21:59:49 +0100

    Create operationFunctionDeclarations.h

---
+++ zad7_MK.c/header_files/operationFunctionDeclarations.h
@@ -0,0 +1,9 @@
+#ifndef OPERATION_FUNCTION_DECLARATION
+#define OPERATION_FUNCTION_DECLARATION
+
+int ReadOneChar(_Stack* head);
+int Sum(_Stack* head);
+int Multiplication(_Stack* head);
+int Subtraction(_Stack* head);
+
+#endif
