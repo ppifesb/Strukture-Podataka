af71a642469b2f1be101b12bc7f1b75096c0368e
ppifesb <56430071+ppifesb@users.noreply.github.com>
2019-12-09 22:00:18 +0100

    Create checkFunctionDeclaration.h

---
+++ zad7_MK.c/header_files/checkFunctionDeclaration.h
@@ -0,0 +1,8 @@
+#ifndef CHECK_FUNCTIONS_DECLARATION
+#define CHECK_FUNCTIONS_DECLARATION
+
+int FileCheck(int);
+int BufferCheck(_Stack* head, char buffer);
+int PrintfAndScanfCheck(int, int);
+
+#endif
