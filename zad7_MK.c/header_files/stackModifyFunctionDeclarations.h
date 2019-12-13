a27847a4d83fbb616667994b8d54cd28f5403c68
ppifesb <56430071+ppifesb@users.noreply.github.com>
2019-12-13 15:45:44 +0100

    Update stackModifyFunctionDeclarations.h

--- zad7_MK.c/header_files/stackModifyFunctionDeclarations.h
+++ zad7_MK.c/header_files/stackModifyFunctionDeclarations.h
@@ -6,5 +6,7 @@ _Stack* Linker(_Stack* previousElement, _Stack* nextElement);
 int Pop(_Stack* head);
 int Push(_Stack* head, int buffer);
 int StoreResultInStack(_Stack* head, int buffer, int oprationNumber);
+int ReadNumberFromString(char* stringBuffer, int option, int* numberOfBytesReadedInTotal);
+int NumberFromStringToStack(_Stack* head, char* stringBuffer);
 
 #endif
